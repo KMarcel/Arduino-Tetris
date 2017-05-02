0b96af25c4d968c273cb82742c9ac1c3d9f8fe6e
Marcel <Marcel_Mich@web.de>
2017-05-02 23:00:23 +0200

    Update Tetris.h

--- Sketch/Tetris/Tetris.h
+++ Sketch/Tetris/Tetris.h
@@ -1,1 +1,11 @@
-
+/*************************************************************************
+ Title	:   Tetris Game
+ Author:    Marrcel Kühl <Marcel.Kuehl@web.de>  https://github.com/KMarcel
+ File:	    Tetris.h / Tetris.cpp
+ Software:  Arduino IDE
+ Hardware:  Arduino Serie 
+***************************************************************************/
+class Tetris{
+Tetris();
+void Draw();
+};
